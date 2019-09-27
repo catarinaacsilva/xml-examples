2bf74b2be29e55f4d12e75c44c969c1033a8d3e1
catarina <c.alexandracorreia@ua.pt>
2019-09-27 23:41:06 +0100

    Updated django project: added ability to enter file names

--- first_example/django_project/app/views.py
+++ first_example/django_project/app/views.py
@@ -1,5 +1,37 @@
 from django.shortcuts import render
 from django.http import HttpRequest, HttpResponse
+from datetime import datetime
+
+def sendinfo(request):
+    assert isinstance(request, HttpRequest)
+    if 'xml' in request.POST and 'schema' in request.POST:
+        xml = request.POST['xml']
+        schema = request.POST['schema']
+        if xml and schema:
+            return render(
+                request,
+                'send_results.html',
+                {
+                    'xml':xml,
+                    'schema':schema,
+                }
+            )
+        else:
+            return render(
+                request,
+                'send_info.html',
+                {
+                    'error':True,
+                }
+            )
+    else:
+        return render(
+            request,
+            'send_info.html',
+            {
+                'error':False,
+            }
+        )
 
 
 
