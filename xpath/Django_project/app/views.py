cf4a34bab2e4905108923029aa8c6a45e6e38ba5
catarina <c.alexandracorreia@ua.pt>
2019-10-03 16:55:48 +0100

    Added template html

--- xpath/Django_project/app/views.py
+++ xpath/Django_project/app/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 import xml.etree.ElementTree as ET
 from django.http import HttpRequest, HttpResponse
+from django.template import loader
 
 
 def info_cursos_xml(xml_file: str):
@@ -14,5 +15,10 @@ def info_cursos_xml(xml_file: str):
 
 
 def show_cursos(request):
-    return HttpResponse(info_cursos_xml('cursos.xml'))
+    #return HttpResponse(info_cursos_xml('cursos.xml'))
+    template = loader.get_template('show_info.html')
+    context = {
+        'info': info_cursos_xml(('cursos.xml')),
+    }
+    return HttpResponse(template.render(context, request))
 
