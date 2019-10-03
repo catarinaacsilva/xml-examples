9d211f393708348a5f86419282b9912428a1a217
catarina <c.alexandracorreia@ua.pt>
2019-10-03 16:42:56 +0100

    Updated readme of xpath

--- xpath/README.md
+++ xpath/README.md
@@ -1,1 +1,13 @@
 # xpath 
+
+## Run
+
+#### info_xml.py
+
+Display content of xml file on terminal
+
+`pythn3 info_xml.py`
+
+#### Django project
+
+Run in pycharm
--- xpath/info_xml.py
+++ xpath/info_xml.py
@@ -1,3 +1,11 @@
+# coding: utf-8
+
+
+__author__ = 'Catarina Silva'
+__version__ = '0.1'
+__email__ = 'c.alexandracorreia@ua.pt'
+__status__ = 'Development'
+
 import xml.etree.ElementTree as ET
 
 def info_cursos_xml(xml_file: str):
