79c0e79a17e06705079c8e85e6a36ff59476de8e
catarina <c.alexandracorreia@ua.pt>
2019-10-10 18:30:14 +0100

    updated django project : xpath

--- README.md
+++ README.md
@@ -1,10 +1,15 @@
 # xml examples
 
-## First example
+## lxml
 
 1. Program in python to:
 
     - read xml doc
     - validate xml doc
     - show info
+
+## xpath
+
+## xqueries
+
 
--- xpath/Django_project/app/views.py
+++ xpath/Django_project/app/views.py
@@ -1,21 +1,23 @@
-from django.shortcuts import render
-
 import xml.etree.ElementTree as ET
-from django.http import HttpRequest, HttpResponse
+
+from django.http import HttpResponse
+from django.shortcuts import render
 from django.template import loader
 
+
 def index(request):
-	return render(request, 'index.html')
+    return render(request, 'index.html')
+
 
 # return a dictionary:
 #   key : name
 #   value: guid
-def info_cursos_xml_dic(xml_file: str, a: str, b:str, c: str):
+def info_cursos_xml_dic(xml_file: str):
     dic = {}
     tree = ET.parse(xml_file)
     root = tree.getroot()
-    for curso in root.findall(a):
-        dic.update({curso.find(b).text : curso.find(c).text})
+    for c in root.findall('curso'):
+        dic.update({c.find('nome').text: c.find('guid').text})
     return dic
 
 
@@ -27,7 +29,7 @@ def info_codigo_xml_dic(xml_file: str):
     tree = ET.parse(xml_file)
     root = tree.getroot()
     for curso in root.findall('curso'):
-        dic.update({curso.find('codigo').text : curso.find('guid').text})
+        dic.update({curso.find('codigo').text: curso.find('guid').text})
     return dic
 
 
@@ -39,46 +41,48 @@ def info_grau_xml_dic(xml_file: str):
     tree = ET.parse(xml_file)
     root = tree.getroot()
     for curso in root.findall('curso'):
-        dic.update({curso.find('nome').text : curso.find('grau').text})
+        dic.update({curso.find('nome').text: curso.find('grau').text})
     return dic
 
-#show name of cursos xml file
+
+# show name of cursos xml file
 def show_cursos(request):
     template = loader.get_template('show_info.html')
 
     context = {
-            'info': info_cursos_xml_dic('cursos.xml'),
+        'info': info_cursos_xml_dic('cursos.xml'),
     }
     return HttpResponse(template.render(context, request))
 
+
+def get_curso(xml_file: str, value: str):
+    dic = {}
+    tree = ET.parse(xml_file)
+    root = tree.getroot()
+    for c in root.findall('curso'):
+        if c.find("guid").text == value:
+            #TODO: porque funciona assim...................
+            departamentos = []
+            for d in c.find('departamentos').findall("departamento"):
+                departamentos.append(d.text)
+            areasCientificas = []
+            for a in c.find('areascientificas').findall("areacientifica"):
+                areasCientificas.append(a.text)
+            dic = {
+                'nome': c.find('nome').text,
+                'codigo': c.find('codigo').text,
+                'grau': c.find('grau').text,
+                'departamento': departamentos,
+                'areaCientifica': areasCientificas,
+                'local': c.find('local').text
+            }
+            break
+    return dic
+
+
 def show_details(request):
     template = loader.get_template('details.html')
-
     value = request.GET.get('item')
-
-    dic_cursos = info_cursos_xml_dic('cursos.xml')
-    dic_codigo = info_codigo_xml_dic('cursos.xml')
-    dic_grau = info_grau_xml_dic('cursos.xml')
-
-    for i in dic_cursos.keys():
-        if dic_cursos.get(i) == value:
-            name = i
-
-    for i in dic_codigo.keys():
-        if dic_codigo.get(i) == value:
-            codigo = i
-
-    for i in dic_grau.keys():
-        if dic_grau.get(i) == value:
-            grau = i
-
-
-    context = {
-        'nome': name,
-        'codigo': codigo,
-        'grau': grau,
-        #'departamento': departamento,
-        #'areaCientifica': areaCientifica,
-        #'local': local
-    }
+    context = get_curso("cursos.xml", value)
+    print(context)
     return HttpResponse(template.render(context, request))
