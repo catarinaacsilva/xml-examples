d3ac7681b2ffba541f6bb6d2928ff85c0633a4b2
catarina <c.alexandracorreia@ua.pt>
2019-10-16 13:20:56 +0100

    Added new features to webapp
    Added a new page where it is possible insert data.

--- xpath/Django_project/app/views.py
+++ xpath/Django_project/app/views.py
@@ -1,9 +1,20 @@
+# coding: utf-8
+
+
+__author__ = 'Catarina Silva'
+__version__ = '0.1'
+__email__ = 'c.alexandracorreia@ua.pt'
+__status__ = 'Development'
+
 import xml.etree.ElementTree as ET
 import lxml.etree as LET #to use on xslt
 import requests
+import sys
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from lxml import etree
+from io import StringIO
 
 
 def index(request):
@@ -168,10 +179,11 @@ def all_locals(request):
 
     return HttpResponse(template.render(context, request))
 
+
 # after show info about curso is possible to see more about it
 # Use xslt to transform xml in html page
 #input:xml --> xslt generate html template --> show information
-def more_info_curso(request):
+def more_info_curso():
     response = requests.get('http://acesso.ua.pt/xml//cursos.v5.asp?')
     if response.status_code == 200:
         receive = LET.fromstring(response.content)
@@ -183,3 +195,76 @@ def more_info_curso(request):
 
     return HttpResponse("Error")
 
+#validate a xml file with xml schema
+def validate(file_xml: str, file_schema: str):
+    # parsing xsd
+    with open(file_schema, 'r') as schema_file:
+        xmlschema_doc = etree.parse(schema_file)
+        xmlschema = etree.XMLSchema(xmlschema_doc)
+
+        # parsing xml
+        with open(file_xml, 'r') as xml_file:
+            try:
+                doc = etree.parse(xml_file)
+                print('XML well formed, syntax ok.')
+
+            # check for file IO error
+            except IOError:
+                print('Invalid File')
+
+            # check for XML syntax errors
+            except etree.XMLSyntaxError as err:
+                print('XML Syntax Error, see error_syntax.log')
+                with open('error_syntax.log', 'w') as error_log_file:
+                    error_log_file.write(str(err.error_log))
+                quit()
+
+            except:
+                print('Unknown error, exiting.')
+                quit()
+
+        # validate against schema
+        try:
+            xmlschema.assertValid(doc)
+            print('XML valid, schema validation ok.')
+
+        except etree.DocumentInvalid as err:
+            print('Schema validation error, see error_schema.log')
+            with open('error_schema.log', 'w') as error_log_file:
+                error_log_file.write(str(err.error_log))
+            quit()
+
+        except:
+            print('Unknown error, exiting.')
+            quit()
+
+        # validate schema
+        try:
+            xmlschema.assertValid(doc)
+            print('XML valid, schema validation ok.')
+
+        except etree.DocumentInvalid as err:
+            print('Schema validation error, see error_schema.log')
+            with open('error_schema.log', 'w') as error_log_file:
+                error_log_file.write(str(err.error_log))
+            quit()
+
+        except:
+            print('Unknown error, exiting.')
+            quit()
+
+
+
+def write_info_xml(request):
+    #read xml file insert in form
+    open(request.POST.get('xml_file'), 'r')
+
+    #get data from template
+    data_to_write = request.POST.get('new_info')
+
+    #copy xml file to another file -> new_xml_file
+
+    # Add data_to_write to file
+
+    #validate new_xml_file with validate function
+    validate()
